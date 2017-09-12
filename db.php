c323451486c1aa1628bf77be4bbc748fd476e5c8
Sarah Albright <Jasmalb@flash.net>
2017-09-11 20:40:10 -0400

    added port to connection string

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
-$server = 'DESKTOP-C174OTB\DEVELOPERSQL';
+$server = 'DESKTOP-C174OTB\DEVELOPERSQL, 49172';
 $connectionInfo = array( "Database"=>"BrewPoint", "UID"=>"testuser", "PWD"=>"testpass");
 $db = sqlsrv_connect( $server, $connectionInfo);
 
-
+//$server = 'DESKTOP-C174OTB\DEVELOPERSQL, 49172';
 
 
 ?>
